5b65fd84adc8491978ea04adf6d490fe12e82de7
Hio Fadlika Akbar <89866871+hiofadlikaakbar@users.noreply.github.com>
2024-08-10 11:55:08 +0700

    refactor

--- script.js
+++ script.js
@@ -2,54 +2,26 @@ const btn = document.querySelectorAll("#btn");
 const currentHours = document.querySelectorAll("#current");
 const previousHours = document.querySelectorAll("#previous");
 
-// daily data
-function dailyData() {
+// Function to update data based on the selected timeframe
+function updateData(timeframe) {
   fetch("./data.json")
     .then((res) => res.json())
-    .then((data) =>
+    .then((data) => {
       currentHours.forEach((e, i) => {
-        currentHours[i].textContent = `${data[i].timeframes.daily.current}hrs`;
+        currentHours[i].textContent = `${data[i].timeframes[timeframe].current}hrs`;
         previousHours[
           i
-        ].textContent = `Last week - ${data[i].timeframes.daily.previous}hrs`;
-      })
-    );
+        ].textContent = `Last week - ${data[i].timeframes[timeframe].previous}hrs`;
+      });
+    });
 }
 
-// weekly data
-function weeklyData() {
-  fetch("./data.json")
-    .then((res) => res.json())
-    .then((data) =>
-      currentHours.forEach((e, i) => {
-        currentHours[i].textContent = `${data[i].timeframes.weekly.current}hrs`;
-        previousHours[
-          i
-        ].textContent = `Last week - ${data[i].timeframes.weekly.previous}hrs`;
-      })
-    );
-}
-
-// monthly data
-function monthlyData() {
-  fetch("./data.json")
-    .then((res) => res.json())
-    .then((data) =>
-      currentHours.forEach((e, i) => {
-        currentHours[
-          i
-        ].textContent = `${data[i].timeframes.monthly.current}hrs`;
-        previousHours[
-          i
-        ].textContent = `Last week - ${data[i].timeframes.monthly.previous}hrs`;
-      })
-    );
-}
-
-// active
+// Add event listeners to buttons
 btn.forEach((butt) => {
   butt.addEventListener("click", function (e) {
     e.preventDefault();
+    const timeframe = this.dataset.timeframe; // assuming each button has a data-timeframe attribute
+    updateData(timeframe);
     btn.forEach((b) => b.classList.remove("text-white"));
     this.classList.add("text-white");
   });
