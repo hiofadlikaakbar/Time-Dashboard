5b91e3c43fd2a9695909c3ccf5c9e140071167e8
Hio Fadlika Akbar <89866871+hiofadlikaakbar@users.noreply.github.com>
2024-08-10 15:32:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,55 @@ const btn = document.querySelectorAll("#btn");
 const currentHours = document.querySelectorAll("#current");
 const previousHours = document.querySelectorAll("#previous");
 
-function updateData(timeframe) {
+// daily data
+function dailyData() {
   fetch("./data.json")
     .then((res) => res.json())
-    .then((data) => {
+    .then((data) =>
       currentHours.forEach((e, i) => {
-        currentHours[i].textContent = `${data[i].timeframes[timeframe].current}hrs`;
-        previousHours[i].textContent = `Last week - ${data[i].timeframes[timeframe].previous}hrs`;
-      });
-    });
+        currentHours[i].textContent = ${data[i].timeframes.daily.current}hrs;
+        previousHours[
+          i
+        ].textContent = Last week - ${data[i].timeframes.daily.previous}hrs;
+      })
+    );
 }
 
-// Adding event listeners to each button for the respective timeframe
+// weekly data
+function weeklyData() {
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) =>
+      currentHours.forEach((e, i) => {
+        currentHours[i].textContent = ${data[i].timeframes.weekly.current}hrs;
+        previousHours[
+          i
+        ].textContent = Last week - ${data[i].timeframes.weekly.previous}hrs;
+      })
+    );
+}
+
+// monthly data
+function monthlyData() {
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) =>
+      currentHours.forEach((e, i) => {
+        currentHours[
+          i
+        ].textContent = ${data[i].timeframes.monthly.current}hrs;
+        previousHours[
+          i
+        ].textContent = Last week - ${data[i].timeframes.monthly.previous}hrs;
+      })
+    );
+}
+
+// active
 btn.forEach((butt) => {
   butt.addEventListener("click", function (e) {
     e.preventDefault();
     btn.forEach((b) => b.classList.remove("text-white"));
     this.classList.add("text-white");
-
-    const timeframe = this.getAttribute("data-timeframe");
-    updateData(timeframe);
   });
 });
