faa350c40699d76b292e0a71bb9ac9f971b0429f
Hio Fadlika Akbar <89866871+hiofadlikaakbar@users.noreply.github.com>
2024-08-10 15:31:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,25 @@ const btn = document.querySelectorAll("#btn");
 const currentHours = document.querySelectorAll("#current");
 const previousHours = document.querySelectorAll("#previous");
 
-// Function to update data based on the selected timeframe
 function updateData(timeframe) {
   fetch("./data.json")
     .then((res) => res.json())
     .then((data) => {
       currentHours.forEach((e, i) => {
         currentHours[i].textContent = `${data[i].timeframes[timeframe].current}hrs`;
-        previousHours[
-          i
-        ].textContent = `Last week - ${data[i].timeframes[timeframe].previous}hrs`;
+        previousHours[i].textContent = `Last week - ${data[i].timeframes[timeframe].previous}hrs`;
       });
     });
 }
 
-// Add event listeners to buttons
+// Adding event listeners to each button for the respective timeframe
 btn.forEach((butt) => {
   butt.addEventListener("click", function (e) {
     e.preventDefault();
-    const timeframe = this.dataset.timeframe; // assuming each button has a data-timeframe attribute
-    updateData(timeframe);
     btn.forEach((b) => b.classList.remove("text-white"));
     this.classList.add("text-white");
+
+    const timeframe = this.getAttribute("data-timeframe");
+    updateData(timeframe);
   });
 });
